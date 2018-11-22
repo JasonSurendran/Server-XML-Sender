10d6aa6ad95b66e08f531be9142682b6b5c4c933
Jason Surendran <surendranjason@gmail.com>
2018-11-21 23:17:49 -0500

    Adding comments

--- DataGather/src/testing/ClientMain.java
+++ DataGather/src/testing/ClientMain.java
@@ -11,6 +11,9 @@ import java.util.Scanner;
 public class ClientMain {
 	public static void main(String[] args) {
 		Scanner scanner = null;
+		
+		//Connecting to Locahost 5000 
+		//Can be set to different value if needed
 		try(Socket socket = new Socket("localhost",5000)){
 			socket.setSoTimeout(5000);
 			BufferedReader echoes = new BufferedReader(
@@ -19,10 +22,13 @@ public class ClientMain {
 			scanner = new Scanner(System.in);
 			String echoString;
 			String response;
-			do {
+			
+			do {	//Asking for tag to be searched
 				System.out.println("Enter Tag To Be Searched: ");
 				echoString = scanner.nextLine();
 				stringToEcho.println(echoString);
+				
+				//Exit command terminates connection
 				if(!echoString.equals("exit")) {
 					response = echoes.readLine();
 					System.out.println(response);}
@@ -32,5 +38,7 @@ public class ClientMain {
 		}catch(IOException e) {
 			System.out.println("IOException: " + e.getMessage());}
 		finally {
+			
+		//Closing Scanner
 		    if(scanner!=null)
 		        scanner.close();}}}
