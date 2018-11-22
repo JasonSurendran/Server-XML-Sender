1d392176e61503f54cd3debaaa23f4496b848546
Jason Surendran <surendranjason@gmail.com>
2018-11-21 23:21:52 -0500

    Update ServerEchoer.java

--- DataGather/src/testing/ServerEchoer.java
+++ DataGather/src/testing/ServerEchoer.java
@@ -9,7 +9,7 @@ import java.net.MalformedURLException;
 import java.net.Socket;
 import java.net.URL;
 
-
+//Creating instance of server that echos back XML data
 public class ServerEchoer extends Thread {
     private Socket socket;
     public ServerEchoer(Socket socket) {
@@ -17,6 +17,7 @@ public class ServerEchoer extends Thread {
 
     @Override
     public void run() {
+        
         try {
             BufferedReader input = new BufferedReader(
                     new InputStreamReader(socket.getInputStream()));
@@ -25,12 +26,15 @@ public class ServerEchoer extends Thread {
                 String echoString = input.readLine();
                 if(echoString.equals("exit")) {
                     break;}
+            //Using sample url to gain XML data from    
             try {
                 URL url = new URL("https://api.flickr.com/services/feeds/photos_public.gne?tags="+echoString);
                 System.out.println(url);
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 connection.setRequestMethod("GET");
                 connection.setRequestProperty("User-Agent", "Chrome");
+                
+                //If no connection is established after 15 seconds time out 
                 connection.setReadTimeout(15000);
                 int responseCode = connection.getResponseCode();
                 if(responseCode != 200) {
@@ -43,9 +47,13 @@ public class ServerEchoer extends Thread {
                 String liner = null;
                 while((liner = inputReader.readLine()) != null) {
                     xml = xml + liner;}
+                
+                //Verify link XML data is coming from
                 System.out.println("Server recieved xml from: " +url);
                 output.println(xml);
                 inputReader.close();
+                
+             //Catch and print any errors   
             } catch(MalformedURLException e) {
                 System.out.println("MalformedURLException: " + e.getMessage());
             } catch(IOException e) {
